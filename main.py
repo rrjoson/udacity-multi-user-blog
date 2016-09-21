42b669a2184b716d480815501926a24c3507816d
rrjoson <rr_joson@yahoo.com>
2016-09-22 04:04:35 +0800

    Add parent blog handler

--- main.py
+++ main.py
@@ -32,7 +32,28 @@ jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),
                                autoescape=True)
 
 
+# Global functions
+
+def render_str(template, **params):
+    t = jinja_env.get_template(template)
+    return t.render(params)
+
+
+# Handlers
+
+class BlogHandler(webapp2.RequestHandler):
+
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        return render_str(template, **params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
 class MainHandler(webapp2.RequestHandler):
+
     def get(self):
         self.response.write('Hello world!')
 
