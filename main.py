eb7447ad376548d237fafc756a1fab5bb9bd9b58
rrjoson <rr_joson@yahoo.com>
2016-09-23 04:37:02 +0800

    Add edit post functionality

--- main.py
+++ main.py
@@ -291,6 +291,41 @@ class NewPostHandler(BlogHandler):
             self.render("newpost.html", subject=subject,
                         content=content, error=error)
 
+class EditPostHandler(BlogHandler):
+
+    def get(self, post_id):
+        key = db.Key.from_path('Post', int(post_id), parent=blog_key())
+        post = db.get(key)
+
+        if self.user and self.user.key().id() == post.user_id:
+            self.render('editpost.html', subject=post.subject,
+                        content=post.content, post_id=post_id)
+
+        elif not self.user:
+            self.redirect('/login')
+
+        else:
+            self.write("You cannot edit this post becuase you are not the one who wrote this post.")
+
+    def post(self, post_id):
+        subject = self.request.get('subject')
+        content = self.request.get('content')
+
+        if subject and content:
+            key = db.Key.from_path('Post', int(post_id), parent=blog_key())
+            post = db.get(key)
+
+            post.subject = subject
+            post.content = content
+
+            post.put()
+
+            self.redirect('/%s' % str(post.key().id()))
+        else:
+            error = "subject and content, please!"
+            self.render("newpost.html", subject=subject,
+                        content=content, error=error)
+
 class PostHandler(BlogHandler):
 
     def get(self, post_id):
@@ -373,5 +408,6 @@ app = webapp2.WSGIApplication([
     ('/newpost', NewPostHandler),
     ('/([0-9]+)', PostHandler),
     ('/([0-9]+)/like', LikePostHandler),
-    ('/([0-9]+)/unlike', UnlikePostHandler)
+    ('/([0-9]+)/unlike', UnlikePostHandler),
+    ('/([0-9]+)/edit', EditPostHandler),
 ], debug=True)
