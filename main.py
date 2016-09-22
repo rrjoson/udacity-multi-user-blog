2705bb5a7010bbe388a698dcf6282e256cdef33d
rrjoson <rr_joson@yahoo.com>
2016-09-23 04:29:46 +0800

    Add unlike functionality

--- main.py
+++ main.py
@@ -317,8 +317,10 @@ class LikePostHandler(BlogHandler):
         elif not self.user:
             self.redirect('/login')
         else:
-            l = Like.all().filter('user_id =', 
-                self.user.key().id()).filter('post_id =', post.key().id()).get()
+            user_id = self.user.key().id()
+            post_id = post.key().id()
+
+            l = Like.all().filter('user_id =', user_id).filter('post_id =', post_id).get()
 
             if l:
                 self.redirect('/' + str(post.key().id()))
@@ -335,6 +337,31 @@ class LikePostHandler(BlogHandler):
 
                 self.redirect('/' + str(post.key().id()))
 
+class UnlikePostHandler(BlogHandler):
+
+    def get(self, post_id):
+        key = db.Key.from_path('Post', int(post_id), parent=blog_key())
+        post = db.get(key)
+
+        if self.user and self.user.key().id() == post.user_id:
+            self.write("You cannot dislike your own post")
+        elif not self.user:
+            self.redirect('/login')
+        else:
+            user_id = self.user.key().id()
+            post_id = post.key().id()
+
+            l = Like.all().filter('user_id =', user_id).filter('post_id =', post_id).get()
+
+            if l:
+                l.delete()
+                post.likes -= 1
+                post.put()
+
+                self.redirect('/' + str(post.key().id()))
+            else:
+                self.redirect('/' + str(post.key().id()))
+
 
 # Routing
 
@@ -345,5 +372,6 @@ app = webapp2.WSGIApplication([
     ('/logout', LogoutHandler),
     ('/newpost', NewPostHandler),
     ('/([0-9]+)', PostHandler),
-    ('/([0-9]+)/like', LikePostHandler)
+    ('/([0-9]+)/like', LikePostHandler),
+    ('/([0-9]+)/unlike', UnlikePostHandler)
 ], debug=True)
