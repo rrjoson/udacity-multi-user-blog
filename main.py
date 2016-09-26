b3c5c7110fb3ccd5837d7afab025c216f6078e42
rrjoson <rr_joson@yahoo.com>
2016-09-27 04:58:51 +0800

    Newpost handler refactoring

--- main.py
+++ main.py
@@ -33,36 +33,8 @@ from handlers.signup import SignupHandler
 from handlers.login import LoginHandler
 from handlers.logout import LogoutHandler
 from handlers.post import PostHandler
+from handlers.newpost import NewPostHandler
 
-
-
-
-
-class NewPostHandler(BlogHandler):
-
-    def get(self):
-        if self.user:
-            self.render("newpost.html")
-        else:
-            error = "You must be signed in to create a post."
-            self.render("base.html", access_error=error)
-
-    def post(self):
-        if not self.user:
-            return self.redirect('/login')
-
-        subject = self.request.get('subject')
-        content = self.request.get('content')
-
-        if subject and content:
-            p = Post(parent=blog_key(), subject=subject,
-                     content=content, user_id=self.user.key().id())
-            p.put()
-            self.redirect('/%s' % str(p.key().id()))
-        else:
-            error = "Please fill up the fields."
-            self.render("newpost.html", subject=subject,
-                        content=content, error=error)
 
 class EditPostHandler(BlogHandler):
 
