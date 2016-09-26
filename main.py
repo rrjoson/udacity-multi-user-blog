7645d66c963bb2076fcb72f9a5c2ddd3599a2d1e
rrjoson <rr_joson@yahoo.com>
2016-09-27 03:47:30 +0800

    Restrict users to be able to edit only thier own posts

--- main.py
+++ main.py
@@ -330,23 +330,27 @@ class EditPostHandler(BlogHandler):
         if not self.user:
             return self.redirect('/login')
 
-        subject = self.request.get('subject')
-        content = self.request.get('content')
+        if self.user and self.user.key().id() == post.user_id:    
+            subject = self.request.get('subject')
+            content = self.request.get('content')
 
-        if subject and content:
-            key = db.Key.from_path('Post', int(post_id), parent=blog_key())
-            post = db.get(key)
+            if subject and content:
+                key = db.Key.from_path('Post', int(post_id), parent=blog_key())
+                post = db.get(key)
 
-            post.subject = subject
-            post.content = content
+                post.subject = subject
+                post.content = content
 
-            post.put()
+                post.put()
 
-            self.redirect('/%s' % str(post.key().id()))
+                self.redirect('/%s' % str(post.key().id()))
+            else:
+                error = "subject and content, please!"
+                self.render("newpost.html", subject=subject,
+                            content=content, error=error)
+
         else:
-            error = "subject and content, please!"
-            self.render("newpost.html", subject=subject,
-                        content=content, error=error)
+            self.write("You cannot edit this post becuase you are not the one who wrote this post.")
 
 class DeletePostHandler(BlogHandler):
 
