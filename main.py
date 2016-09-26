9eb3c6bf9efb758b4bb0fca5f719b0b0dc7d3f27
rrjoson <rr_joson@yahoo.com>
2016-09-27 04:37:09 +0800

    User model refactoring

--- main.py
+++ main.py
@@ -14,111 +14,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import os
-import re
-import random
-import hmac
-import hashlib
+
 import webapp2
-import jinja2
 
-from string import letters
 from google.appengine.ext import db
+from models.user import User
+from helpers import *
 
-# Jinja configuration
-
-template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),
-                               autoescape=True)
-
-
-# Global functions
-
-def render_str(template, **params):
-    t = jinja_env.get_template(template)
-    return t.render(params)
-
-
-# Model keys
-
-def users_key(group='default'):
-    return db.Key.from_path('users', group)
-
-def blog_key(name='default'):
-    return db.Key.from_path('blogs', name)
-
-
-# Validation
-
-USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
-PASS_RE = re.compile(r"^.{3,20}$")
-EMAIL_RE = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
-
-def valid_username(username):
-    return username and USER_RE.match(username)
-
-def valid_password(password):
-    return password and PASS_RE.match(password)
-
-def valid_email(email):
-    return not email or EMAIL_RE.match(email)
-
-
-# Authentication
-
-secret = 'fart'
-
-def make_pw_hash(name, password, salt=None):
-    if not salt:
-        salt = make_salt()
-    h = hashlib.sha256(name + password + salt).hexdigest()
-    return '%s,%s' % (salt, h)
-
-def make_salt(length=5):
-    return ''.join(random.choice(letters) for x in xrange(length))
-
-def valid_pw(name, password, h):
-    salt = h.split(',')[0]
-    return h == make_pw_hash(name, password, salt)
-
-def make_secure_val(val):
-    return '%s|%s' % (val, hmac.new(secret, val).hexdigest())
-
-def check_secure_val(secure_val):
-    val = secure_val.split('|')[0]
-    if secure_val == make_secure_val(val):
-        return val
-
-
-# Models
-
-class User(db.Model):
-    name = db.StringProperty(required=True)
-    pw_hash = db.StringProperty(required=True)
-    email = db.StringProperty()
-
-    @classmethod
-    def register(cls, name, pw, email=None):
-        pw_hash = make_pw_hash(name, pw)
-        return User(parent=users_key(),
-                    name=name,
-                    pw_hash=pw_hash,
-                    email=email)
-
-    @classmethod
-    def login(cls, username, password):
-        u = User.by_name(username)
-        if u and valid_pw(username, password, u.pw_hash):
-            return u
-
-    @classmethod
-    def by_id(cls, uid):
-        return User.get_by_id(uid, parent=users_key())
-
-    @classmethod
-    def by_name(cls, name):
-        u = User.all().filter('name =', name).get()
-        return u
 
 class Post(db.Model):
     subject = db.StringProperty(required=True)
---
+++ models/user.py
@@ -0,0 +1,30 @@
+from google.appengine.ext import db
+from helpers import *
+
+class User(db.Model):
+    name = db.StringProperty(required=True)
+    pw_hash = db.StringProperty(required=True)
+    email = db.StringProperty()
+
+    @classmethod
+    def register(cls, name, pw, email=None):
+        pw_hash = make_pw_hash(name, pw)
+        return User(parent=users_key(),
+                    name=name,
+                    pw_hash=pw_hash,
+                    email=email)
+
+    @classmethod
+    def login(cls, username, password):
+        u = User.by_name(username)
+        if u and valid_pw(username, password, u.pw_hash):
+            return u
+
+    @classmethod
+    def by_id(cls, uid):
+        return User.get_by_id(uid, parent=users_key())
+
+    @classmethod
+    def by_name(cls, name):
+        u = User.all().filter('name =', name).get()
+        return u
