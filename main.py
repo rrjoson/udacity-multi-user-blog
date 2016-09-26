47fd402214db6d8e1465026c5bce8fcccfbe905c
rrjoson <rr_joson@yahoo.com>
2016-09-27 04:42:13 +0800

    Comment model refactoring

--- main.py
+++ main.py
@@ -23,15 +23,9 @@ from google.appengine.ext import db
 from models.user import User
 from models.post import Post
 from models.like import Like
+from models.comment import Comment
 
 from helpers import *
-
-class Comment(db.Model):
-    content = db.TextProperty(required=True)
-    created = db.DateTimeProperty(auto_now_add=True)
-    last_modified = db.DateTimeProperty(auto_now=True)
-    user_id = db.IntegerProperty(required=True)
-    user_name = db.TextProperty(required=True)
 
 
 # Handlers
---
+++ models/comment.py
@@ -0,0 +1,8 @@
+from google.appengine.ext import db
+
+class Comment(db.Model):
+    content = db.TextProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)
+    last_modified = db.DateTimeProperty(auto_now=True)
+    user_id = db.IntegerProperty(required=True)
+    user_name = db.TextProperty(required=True)
