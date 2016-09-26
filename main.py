50fa5c4adab9cabc441de8fa1c51c974ff6a5e0a
rrjoson <rr_joson@yahoo.com>
2016-09-27 05:14:19 +0800

    EditComment handler refactoring

--- main.py
+++ main.py
@@ -39,41 +39,7 @@ from handlers.deletepost import DeletePostHandler
 from handlers.likepost import LikePostHandler
 from handlers.unlikepost import UnlikePostHandler
 from handlers.addcomment import AddCommentHandler
-
-class EditCommentHandler(BlogHandler):
-
-    def get(self, post_id, post_user_id, comment_id):
-        if self.user and self.user.key().id() == int(post_user_id):
-            postKey = db.Key.from_path('Post', int(post_id), parent=blog_key())
-            key = db.Key.from_path('Comment', int(comment_id), parent=postKey)
-            comment = db.get(key)
-
-            self.render('editcomment.html', content=comment.content)
-
-        elif not self.user:
-            self.redirect('/login')
-
-        else:
-            self.write("You don't have permission to edit this comment.")
-
-    def post(self, post_id, post_user_id, comment_id):
-        if not self.user:
-            return
-
-        if self.user and self.user.key().id() == int(post_user_id):
-            content = self.request.get('content')
-
-            postKey = db.Key.from_path('Post', int(post_id), parent=blog_key())
-            key = db.Key.from_path('Comment', int(comment_id), parent=postKey)
-            comment = db.get(key)
-
-            comment.content = content
-            comment.put()
-
-            self.redirect('/' + post_id)
-
-        else:
-            self.write("You don't have permission to edit this comment.")
+from handlers.editcomment import EditCommentHandler
 
 class DeleteCommentHandler(BlogHandler):
 
